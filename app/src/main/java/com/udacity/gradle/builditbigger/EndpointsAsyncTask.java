9860deb5ec6d2d6727af2009146a64dcbd6c4b8a
poison <whoami6716@hotmail.com>
2018-06-22 01:12:43 +0200

    after GCE

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -40,7 +40,7 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
                     // FIXME: 6/20/18
-                    .setRootUrl("http://localhost:8080/_ah/api/")
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
